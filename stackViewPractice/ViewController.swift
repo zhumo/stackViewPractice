9c5c68ed4c4cdfdf3cd6d2f94066e0255822709a
Mo Zhu <mozhu888@gmail.com>
2016-01-13 17:02:04 -0500

    Initial Commit

---
+++ stackViewPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackViewPractice
+//
+//  Created by Mo Zhu on 1/13/16.
+//  Copyright © 2016 Mo Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
